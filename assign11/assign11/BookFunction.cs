c41e5c644b68fd8c392912ee3d9b3e2e1b23e64a
Mohamed <159513882+Mohaxq@users.noreply.github.com>
2025-10-31 18:15:41 +0200

    Add files via upload

---
+++ assign11/assign11/BookFunction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign11
+{
+    
+    public class BookFunctions
+    {
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+        public static string GetAuthors(Book B)
+        {
+            return string.Join(", ", B.Authors);
+        }
+        public static string GetPrice(Book B)
+        {
+            return $"${B.Price}";
+        }
+        public static BookDelegate GetISBN = delegate (Book B)
+        {
+            return B.ISBN;
+        };
+
+        public static Func<Book, string> GetPublicationDate = B => B.PublicationDate.ToShortDateString();
+
+    }
+}
---
+++ assign11/assign11/LibraryEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign11
+{
+    public delegate string BookDelegate(Book B);
+    public static class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> blist,BookDelegate bookFunction)
+        {
+            foreach (Book B in blist)
+            {
+                Console.WriteLine(bookFunction(B));
+            }
+        }
+    }
+}
